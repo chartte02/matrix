84872c2056db17f366d1a696b94d96b336090c7c
chartte02 <awue2941858037@qq.com>
2024-12-28 21:57:05 +0800

    Update modify.cpp

--- modify.cpp
+++ modify.cpp
@@ -1,10 +1,125 @@
 #include"matrix.h"
 
-extern vector<matrix> matlist;//¾ØÕóÊı×é
+extern vector<matrix> matlist;//çŸ©é˜µæ•°ç»„
 
-//Ãû³Æ£º´´ÔìÔö¹ã¾ØÕó
-//¹¦ÄÜ£ºÉú³ÉÊäÈëÁ½¸ö¾ØÕóµÄÔö¹ã¾ØÕó
-//²ÎÊı£ºÊäÈëÁ½¸ömatrixĞÍ¾ØÕó£¬°´ÕÕÇ°ºóË³ĞòÉú³É
+void inverse() {
+    string name;
+    cin>>name;
+    cout << "Please input the name of matrix"<<endl;
+    while(true) {
+        if(matlist[matrix_search(name)].col==matlist[matrix_search(name)].row) {
+            break;
+        }
+        cout <<"The matrix must be square,please input again"<<endl;
+        cin>>name;
+    }
+    string newname;
+    cout << "Please input the name of your new matrix"<<endl;
+    matrix m;
+    m=matlist[matrix_search(name)].matrix_inverse();
+    m.matrix_modify_name(newname);
+    m.matrix_store();
+    return ;
+}
+
+ void intercept() {
+     cout << "Please input the function you want to intercept(column,row or diagonal"<<endl;
+     string input;
+     cin>>input;
+     string name;
+     if(input == "column") {
+         cout << "Please input the name of the matrix you want to intercept:"<<endl;
+         cin>>name;
+         matrix m,temp;
+         temp.matrix_create();
+         cout << "Please input the lower column of the new matrix:"<<endl;
+         int lower,upper;
+         cin>>lower;
+         cout << "Please input the upper column of the new matrix:"<<endl;
+         cin>>upper;
+         m=temp.matrix_intercept_col(matlist[matrix_search(name)],lower, upper);
+         string newname;
+         cout << "Please input the new name of the new matrix:"<<endl;
+         cin>>newname;
+         m.matrix_modify_name(newname);
+         m.matrix_store();
+     }
+     else if(input == "row") {
+         cout << "Please input the name of the matrix you want to intercept:"<<endl;
+         cin>>name;
+         matrix m,temp;
+         temp.matrix_create();
+         cout << "Please input the lower row of the new matrix:"<<endl;
+         int lower,upper;
+         cin>>lower;
+         cout << "Please input the upper row of the new matrix:"<<endl;
+         cin>>upper;
+         m=temp.matrix_intercept_row(matlist[matrix_search(name)],lower, upper);
+         string newname;
+         cout << "Please input the new name of the new matrix:"<<endl;
+         cin>>newname;
+         m.matrix_modify_name(newname);
+         m.matrix_store();
+
+     }
+     else if(input == "diagonal") {
+         while(true) {
+             cout<<"The main diagonal is defined as 0, with positive values above it and negative values below it."<<endl;
+             cout << "Please input the name of the matrix you want to intercept:"<<endl;
+             cin>>name;
+             if(matlist[matrix_search(name)].col==matlist[matrix_search(name)].row) {
+                 break;
+             }
+             cout<<"The matrix must be a square."<<endl;
+         }
+         matrix m,temp;
+         temp.matrix_create();
+         m=temp.matrix_intercept_dia(matlist[matrix_search(name)],matlist[matrix_search(name)].col);
+         string newname;
+         cout << "Please input the new name of the new matrix:"<<endl;
+         cin>>newname;
+         m.matrix_modify_name(newname);
+         m.matrix_store();
+     }
+     else {
+         cout <<"Wrong input!"<<endl;
+     }
+     return;
+ }
+
+void modify() {
+    string name;
+    cout << "Please enter the name of the matrix you want to modify: ";
+    cin >> name; cout <<"Please input the type you want to modify(name or data)" << endl; string choice;
+    cin >> choice;
+    if(cin.good()) {
+        if(choice == "data") {
+            fraction value;
+            cout << "Please enter the value you want to modify"<< endl;
+            cin >> value;
+            cout <<"Please input the row and column number of the data you want to modify."<< endl;
+            int row,column;
+            cin >> row >> column;
+            matlist[matrix_search(name)].matrix_modify_data(row,column,value);
+            matlist[matrix_search(name)].matrix_store();
+        }
+        else if(choice == "name") {
+            cout << "Please enter the new name "<< endl;
+            string newname;
+            cin >> newname;
+            matrix temp = matlist[matrix_search(newname)];
+            matlist[matrix_search(newname)].name="";
+            temp.matrix_store();
+        }
+        return;
+    }
+        cout << "Wrong input!"  << endl;
+    return;
+}
+
+//åç§°ï¼šåˆ›é€ å¢å¹¿çŸ©é˜µ
+//åŠŸèƒ½ï¼šç”Ÿæˆè¾“å…¥ä¸¤ä¸ªçŸ©é˜µçš„å¢å¹¿çŸ©é˜µ
+//å‚æ•°ï¼šè¾“å…¥ä¸¤ä¸ªmatrixå‹çŸ©é˜µï¼ŒæŒ‰ç…§å‰åé¡ºåºç”Ÿæˆ
 matrix matrix::create_argumentation(const matrix&m1,const matrix &m2) {
     matrix temp;
     if(m1.row!=m2.row) {
@@ -13,8 +128,8 @@ matrix matrix::create_argumentation(const matrix&m1,const matrix &m2) {
     }
     temp.row=m1.row;
     temp.col=m1.col+m2.col;
-    temp.matrix_create(row,col);
-    for(int i=0;i<row;i++) {
+    temp.matrix_create(temp.row,temp.col);
+    for(int i=0;i<temp.row;i++) {
         for(int j=0;j<m1.col;j++) {
             temp.data[i][j]=m1.data[i][j];
         }
@@ -26,45 +141,51 @@ matrix matrix::create_argumentation(const matrix&m1,const matrix &m2) {
 }
 
 
-//Ãû³Æ£ºmatrix_modify_data
-//¹¦ÄÜ£ºĞŞ¸ÄiĞĞjÁĞÔªËØ
-//²ÎÊı£ºi£¬j£ºiĞĞjÁĞ
-//     data_:ĞŞ¸ÄµÄÊı¾İÖµ
-void matrix::matrix_modify_data(int i,int j,fraction data_) {
-    int r=i-1,c=j-1;
-    for(int k=0;k<row;k++) {
-        for(int l=0;l<col;l++) {
-            if(k==r && l==c) {
-                data[k][l]=data_;
-                return;
-            }
-        }
-    }
+ //åç§°ï¼šmatrix_modify_data
+ //åŠŸèƒ½ï¼šä¿®æ”¹iè¡Œjåˆ—å…ƒç´ 
+ //å‚æ•°ï¼šiï¼Œjï¼šiè¡Œjåˆ—
+ //     data_:ä¿®æ”¹çš„æ•°æ®å€¼
+ void matrix::matrix_modify_data(int i,int j,fraction data_) {
+     int r=i-1,c=j-1;
+     for(int k=0;k<row;k++) {
+         for(int l=0;l<col;l++) {
+             if(k==r && l==c) {
+                 data[k][l]=data_;
+                 return;
+             }
+         }
+     }
+     return;
+ }
+
+void matrix::matrix_modify_name(string na) {
+    this->name=na;
     return;
 }
 
 
- //Ãû³Æ£ºmatrix_intercept_row½ØÈ¡ĞĞº¯Êı
- //¹¦ÄÜ£º½ØÈ¡´«Èëm¾ØÕó´Ór1µ½r2ĞĞµÄ¾ØÕó
- //²ÎÊı£ºm:´«Èë¾ØÕó
- //    r1£¬r2£ºr1ĞĞµ½r2ĞĞ
-matrix matrix::matrix_intercept_row(const matrix&m,int r1,int r2) {
+
+ //åç§°ï¼šmatrix_intercept_rowæˆªå–è¡Œå‡½æ•°
+ //åŠŸèƒ½ï¼šæˆªå–ä¼ å…¥mçŸ©é˜µä»r1åˆ°r2è¡Œçš„çŸ©é˜µ
+ //å‚æ•°ï¼šm:ä¼ å…¥çŸ©é˜µ
+ //    r1ï¼Œr2ï¼šr1è¡Œåˆ°r2è¡Œ
+ matrix matrix::matrix_intercept_row(const matrix&m,int r1,int r2) {
     int r_1=r1-1,r_2=r2-1;
     r_1=r_1<r_2?r_1:r_2;
     matrix temp;
     temp.matrix_create(r2-r1,m.col);
-    for(int k=r_1;k<=r_2;k++) {
-        for(int t=0;t<col;t++) {
-            temp.data[k-r_1][t]=m.data[k][t];
-        }
-    }
+     for(int k=r_1;k<=r_2;k++) {
+         for(int t=0;t<col;t++) {
+             temp.data[k-r_1][t]=m.data[k][t];
+         }
+     }
     return temp;
-}
+ }
 
 
- //Ãû³Æ£ºmatrix_intercept_col½ØÈ¡ÁĞº¯Êı
- //¹¦ÄÜ£º½ØÈ¡c1µ½c2ĞĞµÄ¾ØÕó²¢´æ´¢
- //²ÎÊı: c1,c2:c1µ½c2ÁĞ
+ //åç§°ï¼šmatrix_intercept_colæˆªå–åˆ—å‡½æ•°
+ //åŠŸèƒ½ï¼šæˆªå–c1åˆ°c2è¡Œçš„çŸ©é˜µå¹¶å­˜å‚¨
+ //å‚æ•°: c1,c2:c1åˆ°c2åˆ—
 matrix matrix::matrix_intercept_col(const matrix&m,int c1,int c2) {
     matrix temp;
     int c_1=c1-1,c_2=c2-1;
@@ -78,10 +199,10 @@ matrix matrix::matrix_intercept_col(const matrix&m,int c1,int c2) {
     return temp;
 }
 
-//Ãû³Æ£ºmatrix_intercept_dia ½ØÈ¡¶Ô½Çº¯Êı
-//¹¦ÄÜ£º½ØÈ¡dÒÔÉÏµÄÊı¾İ£¬ÆäÓàÒÔÁã²¹³ä
-//²ÎÊı£ºd £º¶Ô½ÇÏß £¨¹æ¶¨Ö÷¶Ô½ÇÏßÎª0£¬ÏòÉÏÎªÕı£¬ÏòÏÂÎª¸º£©
-matrix matrix::matrix_intercept_dia(const matrix &m,int d) {
+ //åç§°ï¼šmatrix_intercept_dia æˆªå–å¯¹è§’å‡½æ•°
+ //åŠŸèƒ½ï¼šæˆªå–dä»¥ä¸Šçš„æ•°æ®ï¼Œå…¶ä½™ä»¥é›¶è¡¥å……
+ //å‚æ•°ï¼šd ï¼šå¯¹è§’çº¿ ï¼ˆè§„å®šä¸»å¯¹è§’çº¿ä¸º0ï¼Œå‘ä¸Šä¸ºæ­£ï¼Œå‘ä¸‹ä¸ºè´Ÿï¼‰
+ matrix matrix::matrix_intercept_dia(const matrix &m,int d) {
     matrix temp;
     temp.matrix_create(m.row,m.col);
     if(d>=0) {
